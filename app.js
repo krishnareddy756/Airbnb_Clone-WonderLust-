6f5691fb5a96dc015d9b3636fd27dabafda780ef
krishnareddy756 <saikrishnareddykalvapalli00@gmail.com>
2025-08-14 16:27:48 +0530

    Fix navbar search icon overlap with guests field
    - Moved search button outside of guests field div to prevent overlap
    - Improved CSS spacing for search-field-guests and search-button
    - Search bar now has proper spacing like Airbnb design

--- app.js
+++ app.js
@@ -1,77 +1,75 @@
 if (process.env.NODE_ENV !== "production") {
-  require('dotenv').config();
+  require("dotenv").config();
 }
 console.log(process.env.SECRET);
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
-const ExpressError = require('./utils/ExpressError');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash =  require('connect-flash');
-const listingRouter = require('./routes/listing');
-const reviewRouter = require('./routes/review');
-const userRouter= require('./routes/user');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const ExpressError = require("./utils/ExpressError");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const listingRouter = require("./routes/listing");
+const reviewRouter = require("./routes/review");
+const userRouter = require("./routes/user");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
-const User = require('./models/user');
-const dbUrl=process.env.ATLASDB_URL;
+const User = require("./models/user");
+const dbUrl = process.env.ATLASDB_URL;
 main()
-  .then(() => console.log('Database connected'))
-  .catch(err => console.log(err));
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true,
-  tlsAllowInvalidCertificates: true
-});
-
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    tlsAllowInvalidCertificates: true,
+  });
 }
 
 // App config
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   touchAfter: 24 * 3600, // time period in seconds
   crypto: {
-    secret: process.env.SECRET ,
+    secret: process.env.SECRET,
   },
-  touchAfter:24 * 3600, // time period in seconds
-}) ;
+  touchAfter: 24 * 3600, // time period in seconds
+});
 
-store.on('error', function(e) {
-  console.log('Session store error', e);
+store.on("error", function (e) {
+  console.log("Session store error", e);
 });
 const sessionOptions = {
   store,
   secret: process.env.SECRET, // Use the secret from .env file
-  resave: false, 
-  saveUninitialized: true, 
+  resave: false,
+  saveUninitialized: true,
   cookie: {
-    httpOnly: true, 
-    expires: Date.now() + 24 * 60 * 60 * 1000*7, // 1 day
-    maxAge: 24 * 60 * 60 * 1000*7 // 1 day
-  }
+    httpOnly: true,
+    expires: Date.now() + 24 * 60 * 60 * 1000 * 7, // 1 day
+    maxAge: 24 * 60 * 60 * 1000 * 7, // 1 day
+  },
 };
 
 // app.get('/', (req, res) => {
 //   res.send('HI I AM ROOT');
 // });
 // // Middleware for session and flash messages
-
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -84,8 +82,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   res.locals.currentUser = req.user; // Make currentUser available in all templates
 
   next();
@@ -100,35 +98,33 @@ app.use((req, res, next) => {
 //   res.send(registereduser);
 // });
 
-
 // Use listing routes
-app.use('/listings', listingRouter);
-app.use('/listings/:id/reviews', reviewRouter);
-app.use('/', userRouter);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 // --- Review Routes directly in app.js --- //
 
 const validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
   if (error) {
-    const errMsg = error.details.map(el => el.message).join(',');
+    const errMsg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(400, errMsg);
   } else {
     next();
   }
 };
 
-
 // Error handling
-app.all('*', (req, res, next) => {
-  next(new ExpressError(404, 'Page Not Found'));
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
 });
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render('error', { message });
+  res.status(statusCode).render("error", { message });
 });
 
 // Start server
 app.listen(8080, () => {
-  console.log('Server is running on port 8080');
+  console.log("Server is running on port 8080");
 });
--- routes/listing.js
+++ routes/listing.js
@@ -1,52 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const wrapAsync = require('../utils/wrapAsync');
-const { listingSchema, reviewSchema } = require('../schema.js');
-const ExpressError = require('../utils/ExpressError');
-const Listing = require('../models/listing');
-const { isLoggedIn,isOwner,validateListing } = require('../middleware.js');
-const listingCounteroller= require('../controllers/listings.js');
-const multer = require('multer');
-const { storage } = require('../cloudConfig.js');
+const wrapAsync = require("../utils/wrapAsync");
+const { listingSchema, reviewSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError");
+const Listing = require("../models/listing");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingCounteroller = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
 
+router.route("/").get(wrapAsync(listingCounteroller.index)).post(
+  isLoggedIn,
 
-router.route('/')
-.get( wrapAsync(listingCounteroller.index))
-.post(
-   isLoggedIn,
-   
-   upload.single('listing[image]'), 
-   validateListing, 
-     wrapAsync(listingCounteroller.createListing));
-
+  upload.single("listing[image]"),
+  validateListing,
+  wrapAsync(listingCounteroller.createListing)
+);
 
 //new route
-router.get('/new', isLoggedIn,listingCounteroller.renderNewForm);
+router.get("/new", isLoggedIn, listingCounteroller.renderNewForm);
 
-router.route('/:id')
-.get(wrapAsync(listingCounteroller.showListing))
-.put(
-  isLoggedIn,
-  isOwner,
-  upload.single('listing[image]'),
-  validateListing,
-  wrapAsync(listingCounteroller.updateListing))
-.delete(
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingCounteroller.destroyListing));
-
-
-
-
-
-
+router
+  .route("/:id")
+  .get(wrapAsync(listingCounteroller.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingCounteroller.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingCounteroller.destroyListing));
 
 // Edit Route
-router.get('/:id/edit',isLoggedIn,
+router.get(
+  "/:id/edit",
+  isLoggedIn,
   isOwner,
-   wrapAsync(listingCounteroller.renderEditForm));
-
+  wrapAsync(listingCounteroller.renderEditForm)
+);
 
 module.exports = router;
